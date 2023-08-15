9d697550f7742e07f40ffcc7e28d2e69d0a8290b
Hugo Thunnissen <devel@hugot.nl>
2023-08-15 18:28:50 +0200

    Fix oopsie in compile script

--- compile.bash
+++ compile.bash
@@ -4,6 +4,6 @@ for file in ./*.el; do
     cask emacs -batch -L . --eval '(setq byte-compile-error-on-warn t)' -f batch-byte-compile "$file" || break
 done
 
-if ! [[ -n 'NO_REMOVE_ELC' ]]; then
+if [[ -z $NO_REMOVE_ELC ]]; then
     rm ./*.elc
 fi
